7915440cc39d329b7bd32c001a5a12ab2bf7c9bc
RPatrick98 <99049638@mydavinci.nl>
2020-10-28 09:09:31 +0100

    2

--- Opdracht3/Opdracht3.js
+++ Opdracht3/Opdracht3.js
@@ -1,11 +1,12 @@
-function personal() {
-	var name;
-	var age;
-	var stop;
-	while() {
-		name = prompt ("Hoe heet je?");
-
+function info() {
+	var name 
+	var age
+	while(name,age != 'stop') {
+		name = prompt('Hoe heet je?');	
+		document.write("Hallo " + name + '<br>');
+		age = prompt('Hoe oud ben je?');
+		document.write("Je leeftijd is= " + age + '<br>');
 	}
-	
+
 }
-personal();
+info();
---
+++ Opdracht4/Opdracht4.js
@@ -0,0 +1,10 @@
+var tafel 
+alert("U dient een getal in te voeren");
+
+tafel = prompt("Vul het getal in.");
+while (tafel > 10) {
+	tafel = prompt("Bereik van getal moet zijn: 1 t/m 10");
+}
+for(var i =1; i < 11;i++ ){
+	document.write(i + '*' + tafel + '=' + i*tafel + '<br>');
+}
